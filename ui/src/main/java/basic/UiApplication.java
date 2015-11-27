d1c4025897e4ef64d5672d3e007976699952c9e8
Oliver Trosien <otrosien@epages.com>
2015-11-27 08:10:53 +0100

    code cleanup

--- ui/src/main/java/basic/UiApplication.java
+++ ui/src/main/java/basic/UiApplication.java
@@ -1,18 +1,9 @@
 package basic;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.Sampler;
@@ -20,22 +11,12 @@ import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 @EnableZuulProxy
-public class UiApplication extends WebSecurityConfigurerAdapter {
+public class UiApplication {
 
 	@Bean
 	public Sampler<?> defaultSampler() {
@@ -44,50 +25,6 @@ public class UiApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.logout().and().antMatcher("/**").authorizeRequests()
-		.antMatchers("/index.html", "/home.html", "/", "/login", "/ui/login").permitAll()
-		.anyRequest().authenticated().and().csrf()
-		.csrfTokenRepository(csrfTokenRepository()).and()
-		.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		super.configure(web);
-		web.ignoring().antMatchers("/webjars/**", "/js/**", "/css/**");
-	}
-	
-	private Filter csrfHeaderFilter() {
-		return new OncePerRequestFilter() {
-			@Override
-			protected void doFilterInternal(HttpServletRequest request,
-					HttpServletResponse response, FilterChain filterChain)
-							throws ServletException, IOException {
-				CsrfToken csrf = (CsrfToken) request
-						.getAttribute(CsrfToken.class.getName());
-				if (csrf != null) {
-					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-					String token = csrf.getToken();
-					if (cookie == null
-							|| token != null && !token.equals(cookie.getValue())) {
-						cookie = new Cookie("XSRF-TOKEN", token);
-						cookie.setPath("/");
-						response.addCookie(cookie);
-					}
-				}
-				filterChain.doFilter(request, response);
-			}
-		};
-	}
-
-	private CsrfTokenRepository csrfTokenRepository() {
-		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-		repository.setHeaderName("X-XSRF-TOKEN");
-		return repository;
 	}
 
 }
