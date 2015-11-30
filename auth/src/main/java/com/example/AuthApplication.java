7098b398cb6f6bc5d4811ccb714e343e11f4e4fc
Oliver Trosien <otrosien@epages.com>
2015-11-30 16:38:22 +0100

    re-add form-login (even though it causes the integration tests to fail)

--- auth/src/main/java/com/example/AuthApplication.java
+++ auth/src/main/java/com/example/AuthApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -45,6 +49,33 @@ public class AuthApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+	}
+
+	@Configuration
+	@Order(-20)
+	protected static class LoginConfig extends WebSecurityConfigurerAdapter {
+
+		@Autowired
+		private AuthenticationManager authenticationManager;
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.formLogin().loginPage("/login").permitAll()
+			.and()
+				.requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+			.and()
+				.authorizeRequests()
+				.anyRequest().authenticated()
+			;
+			// @formatter:on
+		}
+
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.parentAuthenticationManager(authenticationManager);
+		}
 	}
 
 	@Configuration
